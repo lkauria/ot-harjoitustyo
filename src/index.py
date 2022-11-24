c4a8c30ee6a6434f00791958c4649137af175cf5
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-24 15:49:10 +0200

    entry fields and a sign in button

--- src/index.py
+++ src/index.py
@@ -1,12 +1,22 @@
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Button, Entry
 
 window = Tk()
 window.title("Budjetointisovellus")
+window.geometry("400x200")
+
+def login():
+    login_label = Label(window, text="Logiikka kirjautumiselle alkaa")
+    login_label.grid(row=4, column=0)
 
 label_welcome = Label(window, text="Terveuloa budjetointisovellukseen!")
-label_login = Label(window, text="Kirjaudu: ")
+button_login = Button(window, text="Kirjaudu", padx=50, pady=20, command=login, bg="#66cdaa")
 
-label_welcome.grid(row=0, column=0)
-label_login.grid(row=3, column=1)
+entry_username = Entry(window, width=60)
+entry_password = Entry(window, width=60)
+
+label_welcome.grid(row=0, column=1)
+button_login.grid(row=3, column=1)
+entry_username.grid(row=1, column=1)
+entry_password.grid(row=2, column=1)
 
 window.mainloop()
