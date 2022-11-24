2aca085a7d613f858add2daa67d8d816f3133a60
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-24 15:25:07 +0200

    add grid

--- src/index.py
+++ src/index.py
@@ -3,7 +3,10 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Budjetointisovellus")
 
-my_label = Label(window, text="Terveuloa budjetointisovellukseen!")
-my_label.pack()
+label_welcome = Label(window, text="Terveuloa budjetointisovellukseen!")
+label_login = Label(window, text="Kirjaudu: ")
+
+label_welcome.grid(row=0, column=0)
+label_login.grid(row=3, column=1)
 
 window.mainloop()
