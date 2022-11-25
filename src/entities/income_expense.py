b28b91514ea192458f6b1eabb758fbcf8cc78cc8
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-25 12:24:57 +0200

    remove service logic from entities

--- src/entities/income_expense.py
+++ src/entities/income_expense.py
@@ -2,13 +2,11 @@ from csv import unregister_dialect
 
 
 class Income_expense:
-    def __init__(self, subject, sum, date_of_transaction, receiver, payer, user_id):
+    def __init__(self, subject, sum, date_of_transaction, user_id):
 
         self.subject = subject
         self.sum = sum
         self.date_of_transaction = date_of_transaction 
-        self.receiver = receiver
-        self.payer = payer
         self.user_id = user_id
-        
-        
+    
+ 
