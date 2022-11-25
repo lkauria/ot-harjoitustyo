af1023131fc30614d4e2cb131ff440056e1a7511
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-25 13:32:48 +0200

    start of transaction service class

--- src/services/transaction_service.py
+++ src/services/transaction_service.py
@@ -1,1 +1,21 @@
-    
+import re
+from entities.user import User
+from entities.transaction import Transaction
+
+class TransactionService:
+    def __init__(self, transaction_repository, user_repository):
+        self._user = None
+        self._transction_repository = transaction_repository
+        self._user_repository = user_repository
+
+    def create_transaction(self, content):
+        transaction = Transaction(content=content, user=self._user)
+
+        return self._transction_repository.create(transaction)
+
+    def get_list_of_transactions(self):
+
+        return
+
+## not ready
+
