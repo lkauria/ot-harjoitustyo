e3ba047341299eafa43321f3fd89672397265667
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-26 12:48:49 +0200

    add dependencies

--- src/index.py
+++ src/index.py
@@ -4,12 +4,15 @@ window = Tk()
 window.title("Budjetointisovellus")
 window.geometry("800x400")
 
+
 def login():
     login_label = Label(window, text="Logiikka kirjautumiselle alkaa")
     login_label.grid(row=4, column=1)
 
+
 label_welcome = Label(window, text="Tervetuloa budjetointisovellukseen!")
-button_login = Button(window, text="Kirjaudu", padx=50, pady=20, command=login, bg="#66CDAA")
+button_login = Button(window, text="Kirjaudu", padx=50,
+                      pady=20, command=login, bg="#66CDAA")
 
 entry_username = Entry(window, width=60)
 entry_password = Entry(window, width=60)
