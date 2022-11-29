4ffe7e3c1828b46e47594173253074e021bc5919
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-29 12:06:32 +0200

    add main() start to index.py, login view still needs to be moved (later)

--- src/index.py
+++ src/index.py
@@ -1,28 +1,33 @@
 from tkinter import Tk, Label, Button, Entry
 
-window = Tk()
-window.title("Budjetointisovellus")
-window.geometry("800x400")
+
+def main():
+    window = Tk()
+    window.title("Budjetointisovellus")
+    window.geometry("800x400")
 
 
-def login():
-    login_label = Label(window, text="Logiikka kirjautumiselle alkaa")
-    login_label.grid(row=4, column=1)
+    def login():
+        login_label = Label(window, text="Logiikka kirjautumiselle alkaa")
+        login_label.grid(row=4, column=1)
 
 
-label_welcome = Label(window, text="Tervetuloa budjetointisovellukseen!")
-button_login = Button(window, text="Kirjaudu", padx=50,
-                      pady=20, command=login, bg="#66CDAA")
+    label_welcome = Label(window, text="Tervetuloa budjetointisovellukseen!")
+    button_login = Button(window, text="Kirjaudu", padx=50,
+                        pady=20, command=login, bg="#66CDAA")
 
-entry_username = Entry(window, width=60)
-entry_password = Entry(window, width=60)
+    entry_username = Entry(window, width=60)
+    entry_password = Entry(window, width=60)
 
-entry_username.insert(0, "käyttäjänimesi ")
-entry_password.insert(0, "salasanasi ")
+    entry_username.insert(0, "käyttäjänimesi ")
+    entry_password.insert(0, "salasanasi ")
 
-label_welcome.grid(row=0, column=1)
-button_login.grid(row=3, column=1)
-entry_username.grid(row=1, column=1)
-entry_password.grid(row=2, column=1)
+    label_welcome.grid(row=0, column=1)
+    button_login.grid(row=3, column=1)
+    entry_username.grid(row=1, column=1)
+    entry_password.grid(row=2, column=1)
 
-window.mainloop()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
