09412d196fdce734ecfabb43d6ce2fb761775fe6
Laura Kauria <laura.kauria@helsinki.fi>
2023-01-01 14:37:37 +0200

    test create user, user exists

--- src/tests/user_service_test.py
+++ src/tests/user_service_test.py
@@ -1,15 +1,17 @@
 import unittest
 from services.user_service import UserService
-from repositories.user_repository import UserRepository
-from entities.user import User
+from errors import UsernameExistsError
 
 class TestUserService(unittest.TestCase):
 
     def test_create_user(self):
         user_service = UserService()
-        user_service.create_user("usern3", "passw1")
+        user_service.create_user("user3", "passw1")
         for user in user_service.list_all_users():
-            if user.get_username() == "user3":
-                pass
+            self.assertEqual(user.get_username(), "user3")
 
-    
+    def test_create_user_but_exists_already(self):
+        user_service = UserService()
+        user_service.create_user("user4", "passw1")
+        self.assertRaises(UsernameExistsError, user_service.create_user, "user4", "passw1")
+
